c1eb8a63bf14a37f8b619259dc0fbc25b7f9566f
Damian Krawczyk <damian@damiankrawczyk.com>
2024-07-15 20:51:11 +0200

    html_baseurl

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,8 @@ html_theme = "shibuya"
 html_static_path = ['_static']
 
 html_title = "LimberDuck"
+html_baseurl = "https://limberduck-org.readthedocs.io/"
+sitemap_url_scheme = "{link}"
 
 html_logo = "_static/img/LimberDuck-logo.png"
 

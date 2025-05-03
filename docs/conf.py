e17a32929c75a0f73ef885b9ee819a1a9b7cc209
Damian Krawczyk <damian@damiankrawczyk.com>
2025-05-03 13:44:47 +0200

    html_baseurl fix

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ html_theme = "shibuya"
 html_static_path = ['_static']
 
 html_title = "LimberDuck"
-html_baseurl = "https://limberduck.org/en/latests"
+html_baseurl = "https://limberduck.org/en/latest"
 sitemap_url_scheme = "{link}"
 
 html_logo = "_static/img/LimberDuck-logo.png"
